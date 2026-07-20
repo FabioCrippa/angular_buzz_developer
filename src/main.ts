5d1b39df5935f16463926b5affbdb2389b847760
Fabio Crippa <crippa.fabiocrippa@gmail.com>
2026-07-20 15:01:43 -0300

    chore: remove temporary trigger comment

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Trigger Vercel rebuild - PSS 2027 deployment
-
 // ✅ Suprimir console.log em produção
 if (environment.production) {
   console.log = () => {};
